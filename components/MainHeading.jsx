1852bb42f866afdb76224f34b9b4e7a34f3c5115
Muhammad-Haris-3 <hariskhokhar975@gmail.com>
2025-07-01 04:19:11 +0500

    third commit

--- components/MainHeading.jsx
+++ components/MainHeading.jsx
@@ -1,4 +1,5 @@
 // import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
 const MainHeading = ({ text }) => {
